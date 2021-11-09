447f38e69ba231563aa68b39d51234974ad3eb5f
Labrot <sarah.lientz@gmail.com config --global user.email>
2021-11-09 12:56:07 -0500

    cleaned up variables in javascript

--- script.js
+++ script.js
@@ -4,46 +4,43 @@
 document.querySelector("#generate").addEventListener("click", writePassword);
 
 // Arrays
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // variables
 var confirmLength = "";
-var confirmSpecialCharacter;
-var confirmNumericCharacter;
-var confirmUpperCase;
-var confirmLowerCase;
+var confirmSpecialCharacter, confirmNumericCharacter, confirmUpperCase, confirmLowerCase;
 
 // Prompt to confirm how many characters the user would like in their password
 function generatePassword() {
-  var confirmLength = (prompt("How long of a password would you like (8-30)?"));
+  confirmLength = (prompt("How long of a password would you like (8-30)?"));
 
   // Loop if answer is outside the parameters 
   while(confirmLength <= 7 || confirmLength >= 31) {
       alert("Your password must be between 8-30 characters. Please try again.");
-      var confirmLength = (prompt("State how many characters you would like in your password: "));
+      confirmLength = (prompt("State how many characters you would like in your password: "));
       } 
 
   // Repeat back how many charactes the user will have  
       alert(`Your password will have ${confirmLength} characters`);
 
     // Determine specification of password 
-    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+    confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+    confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+    confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+    confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
       
     
     
     // Loop if none of the specification are selected
       while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
         alert("You must choose at least one");
-        var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-        var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
+        confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+        confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+        confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+        confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
     } 
 
       // Assign an action to the password parameters NEED TO FIX THIS
@@ -84,3 +81,10 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+document.getElementById("generate").addEventListener("mouseenter", function() {
+  document.getElementById("generate").style.backgroundColor = "blue";
+})
+document.getElementById("generate").addEventListener("mouseleave", function() {
+  document.getElementById("generate").style.backgroundColor = "hsl(360, 91%, 36%)";
+})
